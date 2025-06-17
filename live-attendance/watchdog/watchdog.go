8d12598bb4d52e263f0bae1a34de20b6492f5390
heinz <heinz@42nice.fr>
2025-06-17 18:45:19 +0200

    [LOGS/ERRORS] Improve logs and update API Package to fix 502 errors

--- live-attendance/watchdog/watchdog.go
+++ live-attendance/watchdog/watchdog.go
@@ -71,9 +71,9 @@ func GetAllowEvents() bool {
 
 func AllowEvents(isAllowed bool) {
 	if isAllowed {
-		Log("[WATCHDOG] 🟢 accepting incomming events")
+		Log("[WATCHDOG] 🟢 accepting incoming events")
 	} else {
-		Log("[WATCHDOG] 🔴 refusing incomming events")
+		Log("[WATCHDOG] 🔴 refusing incoming events")
 	}
 	acceptEventsMutex.Lock()
 	acceptEvents = isAllowed
@@ -170,34 +170,53 @@ func UpdateUserAccess(userID int, accessControlUsername string, timeStamp time.T
 func PrintUsersTimers() {
 	parisLoc, _ := time.LoadLocation("Europe/Paris")
 	AllUsersMutex.Lock()
+	defer AllUsersMutex.Unlock()
+
 	total := len(AllUsers)
 	i := 1
 	if total == 0 {
 		Log("[WATCHDOG] No users saved")
-	} else {
-		Log("[WATCHDOG] ┌─ Users status:")
+		return
 	}
-	for _, value := range AllUsers {
-		box := getBoxChar(i, total)
-		if value.FirstAccess.IsZero() {
-			Log(fmt.Sprintf("[WATCHDOG] %s %8s: %s ┆ Total : %s\n",
-				box,
-				value.Login42,
-				"  No badge usage yet  ",
-				formatDuration(value.Duration),
-			))
+
+	Log("[WATCHDOG] ┌─ Users status:")
+
+	var noBadgeUsers, badgeUsers []User
+	for _, user := range AllUsers {
+		if user.FirstAccess.IsZero() {
+			noBadgeUsers = append(noBadgeUsers, user)
 		} else {
-			Log(fmt.Sprintf("[WATCHDOG] %s %8s: %s -> %s ┆ Total : %s\n",
-				box,
-				value.Login42,
-				value.FirstAccess.In(parisLoc).Format("15h04m05s"),
-				value.LastAccess.In(parisLoc).Format("15h04m05s"),
-				formatDuration(value.Duration),
-			))
+			badgeUsers = append(badgeUsers, user)
 		}
+	}
+
+	for _, user := range noBadgeUsers {
+		boxChar := getBoxChar(i, total)
+		Log(fmt.Sprintf("[WATCHDOG] %s %8s: %s ┆ Total : %s\n",
+			boxChar,
+			user.Login42,
+			"  No badge usage yet  ",
+			formatDuration(user.Duration),
+		))
 		i++
 	}
-	AllUsersMutex.Unlock()
+
+	if len(badgeUsers) > 0 {
+		Log("[WATCHDOG] ├──────── Users that badged today")
+	}
+
+	i--
+	for _, user := range badgeUsers {
+		boxChar := getBoxChar(i, total)
+		Log(fmt.Sprintf("[WATCHDOG] %s %8s: %s -> %s ┆ Total : %s\n",
+			boxChar,
+			user.Login42,
+			user.FirstAccess.In(parisLoc).Format("15h04m05s"),
+			user.LastAccess.In(parisLoc).Format("15h04m05s"),
+			formatDuration(user.Duration),
+		))
+		i++
+	}
 }
 
 func isProjectOngoing(login string, projectID string) bool {
@@ -242,68 +261,153 @@ func getBoxChar(i int, len int) string {
 	}
 }
 
+func formatPostInfo(logs map[string][]string, errorType string, user User, loc *time.Location, emoji string, msg string) map[string][]string {
+	apprenticeEmoji := "👤"
+	if user.IsApprentice {
+		apprenticeEmoji = "🎓"
+	}
+	logs[apprenticeEmoji+errorType] = append(logs[apprenticeEmoji+errorType], fmt.Sprintf(
+		"[WATCHDOG] [POST] ├── %s %-8s: %s-%s (%s) — %s\n",
+		emoji,
+		user.Login42,
+		user.FirstAccess.In(loc).Format("15:04:05"),
+		user.LastAccess.In(loc).Format("15:04:05"),
+		formatDuration(user.Duration),
+		msg,
+	))
+	return logs
+}
+
+const NO_BADGE string = "No badge"
+const BADGED_ONCE string = "Badged once"
+const NOT_APPRENTICE string = "Not apprentice"
+const POSTED string = "posted"
+const OTHER string = "Other"
+
+func resetUserDuration(user User) {
+	user.FirstAccess = time.Time{}
+	user.LastAccess = time.Time{}
+	user.Duration = 0
+	AllUsers[user.ControlAccessID] = user
+}
+
 func PostApprenticesAttendances() {
-	msg := ""
+	parisLoc, _ := time.LoadLocation("Europe/Paris")
+	logs := map[string][]string{}
 	AllUsersMutex.Lock()
+	defer AllUsersMutex.Unlock()
 	total := len(AllUsers)
-	i := 1
+	i := 0
 	if total == 0 {
-		Log("[WATCHDOG] Posting Attendances: no users registered")
+		Log("[WATCHDOG] [POST] Posting Attendances: no users registered")
 		return
 	}
-	Log("[WATCHDOG] ┌─ Posting Attendances:")
-	for _, value := range AllUsers {
-		var resp *http.Response
-		box := getBoxChar(i, total)
-		if value.FirstAccess.IsZero() {
-			i++
+	Log("[WATCHDOG] [POST] ┌─ Posting Attendances:")
+	for _, user := range AllUsers {
+		i++
+		if user.FirstAccess.IsZero() {
+			logs = formatPostInfo(logs, NO_BADGE, user, parisLoc, "❌", "No badge usage yet")
+			resetUserDuration(user)
 			continue
 		}
-		id42, err := strconv.ParseInt(value.ID42, 10, 64)
-		if err != nil {
-			msg = fmt.Sprintf("couldn't convert string to int \"%s\"", value.ID42)
-		} else {
-			_ = id42
-			if value.FirstAccess == value.LastAccess {
-				msg = "(Used badge only once)"
-				err = fmt.Errorf("badge error")
-			} else if value.Duration < time.Duration(60*10) {
-				msg = fmt.Sprintf("(too short duration %.2f minutes)", value.Duration.Minutes())
-				err = fmt.Errorf("badge error")
-			} else if !value.IsApprentice {
-				msg = "user is not an apprentice"
-				err = fmt.Errorf("user is not an apprentice")
-			} else if config.ConfigData.Attendance42.AutoPost {
-				resp, err = apiManager.GetClient(config.FTAttendance).Post("/attendances", APIAttendance{
-					Begin_at:  value.FirstAccess.UTC().Format(time.RFC3339),
-					End_at:    value.LastAccess.UTC().Format(time.RFC3339),
-					Source:    "access-control",
-					Campus_id: 41,
-					User_id:   int(id42),
-				})
-				if err != nil {
-					msg = err.Error()
-				} else if resp.StatusCode != http.StatusOK {
-					msg = resp.Status
-					err = fmt.Errorf("invalid status code")
-				} else {
-					msg = resp.Status
-				}
-			} else {
-				msg = "AUTOPOST is OFF"
-				err = fmt.Errorf("autopost is off")
-			}
+
+		id42, _ := strconv.ParseInt(user.ID42, 10, 64)
+
+		if user.FirstAccess == user.LastAccess {
+			logs = formatPostInfo(logs, BADGED_ONCE, user, parisLoc, "❌", "Used badge only once")
+			resetUserDuration(user)
+			continue
 		}
-		if err != nil {
-			Log(fmt.Sprintf("[WATCHDOG] %s ❌ Posted attendance for %-8s (%s): %s\n", box, value.Login42, formatDuration(value.Duration), msg))
-		} else {
-			Log(fmt.Sprintf("[WATCHDOG] %s ✅ Posted attendance for %-8s (%s): %s\n", box, value.Login42, formatDuration(value.Duration), msg))
+
+		// if user.Duration < time.Duration(60*10) {
+		// 	logs = formatPostInfo(logs, OTHER, user, parisLoc, "❌", fmt.Sprintf("(too short duration %.2f minutes)", user.Duration.Minutes()))
+		// 	resetUserDuration(user)
+		// 	continue
+		// }
+
+		if !user.IsApprentice {
+			logs = formatPostInfo(logs, NOT_APPRENTICE, user, parisLoc, "❌", "user is not an apprentice")
+			resetUserDuration(user)
+			continue
 		}
-		value.FirstAccess = time.Time{}
-		value.LastAccess = time.Time{}
-		value.Duration = value.FirstAccess.Sub(value.LastAccess)
-		AllUsers[value.ControlAccessID] = value
-		i++
+
+		if !config.ConfigData.Attendance42.AutoPost {
+			logs = formatPostInfo(logs, POSTED, user, parisLoc, "❌", "AUTOPOST is OFF")
+			resetUserDuration(user)
+			continue
+		}
+
+		resp, err := apiManager.GetClient(config.FTAttendance).Post("/attendances", APIAttendance{
+			Begin_at:  user.FirstAccess.UTC().Format(time.RFC3339),
+			End_at:    user.LastAccess.UTC().Format(time.RFC3339),
+			Source:    "access-control",
+			Campus_id: 41,
+			User_id:   int(id42),
+		})
+
+		if err != nil {
+			logs = formatPostInfo(logs, POSTED, user, parisLoc, "❌", err.Error())
+			resetUserDuration(user)
+			continue
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			logs = formatPostInfo(logs, POSTED, user, parisLoc, "❌", resp.Status)
+			resetUserDuration(user)
+			continue
+		}
+
+		logs = formatPostInfo(logs, POSTED, user, parisLoc, "✅", resp.Status)
+		resetUserDuration(user)
 	}
-	AllUsersMutex.Unlock()
+
+	if len(logs["👤"+NO_BADGE]) > 0 {
+		Log("[WATCHDOG] [POST] ├──────── Students: No badge used today")
+		for _, line := range logs["👤"+NO_BADGE] {
+			Log(line)
+		}
+	}
+
+	if len(logs["👤"+BADGED_ONCE]) > 0 {
+		Log("[WATCHDOG] [POST] ├──────── Students: Used badge only once")
+		for _, line := range logs["👤"+BADGED_ONCE] {
+			Log(line)
+		}
+	}
+
+	if len(logs["👤"+NOT_APPRENTICE]) > 0 {
+		Log("[WATCHDOG] [POST] ├──────── Students: Not an apprentice")
+		for _, line := range logs["👤"+NOT_APPRENTICE] {
+			Log(line)
+		}
+	}
+
+	if len(logs["👤"+OTHER]) > 0 {
+		Log("[WATCHDOG] [POST] ├──────── Students: Other issues")
+		for _, line := range logs["👤"+OTHER] {
+			Log(line)
+		}
+	}
+
+	if len(logs["🎓"+NO_BADGE]) > 0 {
+		Log("[WATCHDOG] [POST] ├──────── Apprentices: No badge used today")
+		for _, line := range logs["🎓"+NO_BADGE] {
+			Log(line)
+		}
+	}
+
+	if len(logs["🎓"+BADGED_ONCE]) > 0 {
+		Log("[WATCHDOG] [POST] ├──────── Apprentices: Used badge only once")
+		for _, line := range logs["🎓"+BADGED_ONCE] {
+			Log(line)
+		}
+	}
+
+	if len(logs["🎓"+POSTED]) > 0 {
+		Log("[WATCHDOG] [POST] ├──────── Apprentices: Posts")
+		for _, line := range logs["🎓"+POSTED] {
+			Log(line)
+		}
+	}
+	Log("[WATCHDOG] [POST] └── Done")
 }
